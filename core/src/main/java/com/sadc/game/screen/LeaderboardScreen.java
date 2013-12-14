7e98fae9dcd296b1b6b62b434568ba43031243ac
t590035 <iceeagle85@gmail.com>
2013-12-13 22:50:47 -0600

    leaderboard fine tuning

--- core/src/main/java/com/sadc/game/screen/LeaderboardScreen.java
+++ core/src/main/java/com/sadc/game/screen/LeaderboardScreen.java
@@ -3,6 +3,7 @@ package com.sadc.game.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
@@ -16,10 +17,15 @@ public class LeaderboardScreen extends GameScreen {
     private FileHandle leaderboardFile;
     private final FreeTypeFontGenerator generator;
     private final BitmapFont font;
+    private final BitmapFont titleFont;
     private final SpriteBatch spriteBatch;
-    private final float height = 400;
+    private final float height = 350;
+    private final Texture chavPicture;
+    private final Texture chav2Picture;
     private List<LeaderboardListing> leaderboardList = new ArrayList<LeaderboardListing>();
     public LeaderboardScreen(){
+        chavPicture = new Texture("chav.jpg");
+        chav2Picture = new Texture("chav2.jpg");
         leaderboardFile = Gdx.files.internal("leaderboard.txt");
         String text = leaderboardFile.readString();
         String[] leaderboardStrings = text.split(":");
@@ -31,18 +37,22 @@ public class LeaderboardScreen extends GameScreen {
             leaderboardListing.setScore(leaderboardItem[1]);
             leaderboardList.add(i, leaderboardListing);
         }
-        generator = new FreeTypeFontGenerator(Gdx.files.internal("Minecraftia.ttf"));
-        font = generator.generateFont(12);
+        generator = new FreeTypeFontGenerator(Gdx.files.internal(GameConstants.LONDON_FONT));
+        titleFont = generator.generateFont(40);
+        font = generator.generateFont(GameConstants.MENU_FONT_SIZE);
         spriteBatch = new SpriteBatch();
     }
     @Override
     public void draw(float delta) {
         Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
         spriteBatch.begin();
+        spriteBatch.draw(chavPicture, 80, 200, 150, 150);
+        spriteBatch.draw(chav2Picture, 450, 185, 125, 175);
+        titleFont.draw(spriteBatch, "Top Chavs", 250, 400);
         float leaderboardHeight = height;
         for (LeaderboardListing leaderboardListing: leaderboardList){
-            font.draw(spriteBatch, leaderboardListing.getName(), 100, leaderboardHeight);
-            font.draw(spriteBatch, leaderboardListing.getScore(), 200, leaderboardHeight);
+            font.draw(spriteBatch, leaderboardListing.getName(), 250, leaderboardHeight);
+            font.draw(spriteBatch, leaderboardListing.getScore(), 350, leaderboardHeight);
             leaderboardHeight -= 20;
         }
         spriteBatch.end();

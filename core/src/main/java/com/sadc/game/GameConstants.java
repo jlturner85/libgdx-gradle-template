0a1803610fd951c66d62b05fcf156c11608648c8
t590035 <iceeagle85@gmail.com>
2013-12-15 13:33:53 -0600

    level select now selects level

--- core/src/main/java/com/sadc/game/GameConstants.java
+++ core/src/main/java/com/sadc/game/GameConstants.java
@@ -60,4 +60,5 @@ public class GameConstants {
     public static final int CAR1_SPRITE_COLUMNS = 2;
     public static final float CAR1_FRAME_DURATION = 0.1f;
     public static int CURRENT_SELECTED_CAR;
+    public static int CURRENT_SELECTED_LEVEL;
 }
